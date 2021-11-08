772772ade42174944d79b00f9255f7b5e3f7d9b4
subhash <subhashpal2020@gmail.com>
2021-11-08 22:58:17 +0530

    Liquid Pagers

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cuberto.liquid_swipe.LiquidPager;
+
+public class MainActivity extends AppCompatActivity {
+LiquidPager pager;
+ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        pager=findViewById(R.id.pager);
+        viewPager=new ViewPager(getSupportFragmentManager(),1);
+        pager.setAdapter(viewPager);
+
+    }
+}
